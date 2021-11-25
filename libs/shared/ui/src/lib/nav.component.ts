12b0506a3ddb3f1c0ba899eed5c3061b55592838
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-11-25 14:42:29 -0500

    add - lib shared/ui, test nav component, storybook/cypress

---
+++ libs/shared/ui/src/lib/nav.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'nx-jest-unit-test-fun-nav',
+  template: `
+    <p>
+      nav works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
