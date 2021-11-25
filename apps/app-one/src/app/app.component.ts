f54dcff23de9bc3ecd54ed3e8a11a748a31c27b4
BalcomIV <35431931+balcomiv@users.noreply.github.com>
2021-11-25 12:29:20 -0500

    initial commit

---
+++ apps/app-one/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nx-jest-unit-test-fun-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'app-one';
+}
